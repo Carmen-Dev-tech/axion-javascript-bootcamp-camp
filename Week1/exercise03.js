60c101854432f190f337657f99708ccc2ab31264
Carmen <carmenlekholo@gmail,com>
2026-07-15 18:19:24 +0200

    Complete Exercise 03

---
+++ Week1/exercise03.js
@@ -0,0 +1,25 @@
+// Exercise 3: Calculator
+
+// Store the first number 
+let num1 = 20;
+
+// Store the second number 
+let num2 = 10;
+
+// Calculate the sum
+let sum = num1 + num2;
+
+// Calculate the differece 
+let differece = num1 - num2;
+
+// Calculate the product 
+let product = num1 * num2;
+
+// Calculate the quotient 
+let quotient = num1 / num2;
+
+// Display the results 
+console.log(sum);
+console.log(differece);
+console.log(product);
+console.log(quotient);

2ca1a80e0d9f61c24fd44fc5cb2ff38dcdcf49b0
Carmen <carmenlekholo@gmail,com>
2026-07-16 22:23:07 +0200

    Complete Exercise 04

---
+++ Week1/exercise04.js
@@ -0,0 +1,10 @@
+// Exercise 4: Temperature Converter
+
+// Store the temperature in Celsius
+let celsius = 25;
+
+// Convert Celsius to Fahrenheit
+let fahrenheit = (celsius * 9 / 5) + 32;
+
+// Display the result
+console.log( fahrenheit);

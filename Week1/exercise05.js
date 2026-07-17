aad16655172baad094ee6983d81a0c1cee5038bf
Carmen <carmenlekholo@gmail,com>
2026-07-17 16:24:52 +0200

    Complete Exercise 05

---
+++ Week1/exercise05.js
@@ -0,0 +1,22 @@
+// Exercise 5: Swap Variables
+
+// Store the first value
+let firstNum = 10;
+
+// Store the second value
+let secondNum = 20;
+
+// Display the values before swapping 
+console.log("Before swapping:");
+console.log(firstNum);
+console.log(secondNum);
+
+// Use a temporary variable to swap the values 
+let temp = firstNum;
+firstNum = secondNum;
+secondNum = temp;
+
+// Display the values after swapping 
+console.log("After swapping:");
+console.log(firstNum);
+console.log(secondNum);

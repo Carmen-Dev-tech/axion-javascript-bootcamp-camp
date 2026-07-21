deeff78bdc4a80698255f460a95a653a4bc1e5ef
Carmen <carmenlekholo@gmail,com>
2026-07-21 17:07:43 +0200

    Complete Exercise 07

---
+++ Week2/exercise07.js
@@ -0,0 +1,13 @@
+// Exercise 7: Grade Calculator 
+
+// Store the student's mark
+let mark = 78;
+
+// Check the student's grade
+if (mark >= 75) {
+    console.log(mark + " - Distinction");
+} else if (mark >= 50) {
+    console.log(mark + " - Pass");
+} else {
+    console.log(mark + " - Fail");
+}

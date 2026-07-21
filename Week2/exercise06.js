6cbdf68f24220d485be5a4ca541a6569e5509035
Carmen <carmenlekholo@gmail,com>
2026-07-21 16:42:12 +0200

    Complete Exercise 06

---
+++ Week2/exercise06.js
@@ -0,0 +1,11 @@
+// Exercise 6: Even or Odd
+
+// Store a number
+let num = 8;
+
+// Check if the number is even or odd
+if (num % 2 === 0) {
+    console.log(num + " is Even");
+} else {
+    console.log(num + " is odd");
+}

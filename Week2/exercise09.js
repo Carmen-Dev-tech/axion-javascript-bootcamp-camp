fceab069521d4b3e9d7849fd21a287aa628e5fe2
Carmen <carmenlekholo@gmail,com>
2026-07-23 12:41:43 +0200

    Complete exercise 09

---
+++ Week2/exercise09.js
@@ -0,0 +1,15 @@
+// Exercise 9: Largest of 3 Numbers 
+
+// Store three numbers 
+let num1 = 10;
+let num2 = 25;
+let num3 = 18;
+
+// Check which number is the largest
+if (num1 >= num2 && num1 >= num3) {
+    console.log(num1 + " is the largest");
+} else if (num2 >= num1 && num2 >= num3) {
+    console.log(num2 + " is the largest"); 
+} else {
+    console.log(num3 + " is the largest");
+}

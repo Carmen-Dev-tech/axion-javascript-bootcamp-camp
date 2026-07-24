fa48f291df67cabd43538ba1b2da2b5d461cc903
Carmen <carmenlekholo@gmail,com>
2026-07-24 09:28:08 +0200

    Complete exercise 10

---
+++ Week2/exercise10.js
@@ -0,0 +1,25 @@
+// exercise 10: Menu Switch
+
+// Store the menu choice 
+let choice = 2;
+
+switch (choice) {
+    case 1:
+    console.log("Burger");
+    break;
+
+    case 2:
+        console.log("Pizza");
+        break;
+
+    case 3:
+        console.log("Pasta");
+        break;
+        
+    case 4:
+        console.log("Salad");
+        break;
+    
+        default:
+            console.log("Invalid choice");
+}

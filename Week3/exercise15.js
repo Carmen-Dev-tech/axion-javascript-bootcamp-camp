fe414bcfca101417ab9be39d2416ab2380f23310
Carmen <carmenlekholo@gmail,com>
2026-08-03 13:54:25 +0200

    Complete Exercise 15

---
+++ Week3/exercise15.js
@@ -0,0 +1,15 @@
+// Exercise 15: Fibonacci Sequence 
+
+let first = 0;
+let second = 1;
+
+console.log(first);
+console.log(second);
+
+for (let i = 3; i <= 10; i++) {
+    let next = first + second;
+    console.log(next);
+
+    first = second;
+    second = next;
+}

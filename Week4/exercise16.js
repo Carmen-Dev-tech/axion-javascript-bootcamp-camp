fda4539f91f5f0bbf68893856c21504a2f9bd61c
Carmen <carmenlekholo@gmail,com>
2026-08-05 17:53:32 +0200

    Complete Exercise 16

---
+++ Week4/exercise16.js
@@ -0,0 +1,11 @@
+// Exercise 16: Square Pattern
+
+for (let row = 1; row <= 5; row++) {
+    let pattern = "";
+
+    for (let column = 1; column <= 5; column++) {
+        pattern += "*";
+    }
+
+    console.log(pattern);
+}

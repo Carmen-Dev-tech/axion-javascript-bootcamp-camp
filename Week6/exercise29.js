da1b667ca7b8cde89620b50faef8c7b4be53987e
Carmen <carmenlekholo@gmail,com>
2026-09-01 14:22:58 +0200

    Complete Exercise 29

---
+++ Week6/exercise29.js
@@ -0,0 +1,15 @@
+// Exercise 29: Remove Duplicates
+
+let numbers = [1, 2, 2, 3, 4, 4, 5];
+
+let uniqueNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (!
+uniqueNumbers.includes(numbers[i])) {
+
+uniqueNumbers.push(numbers[i]);
+        }
+}
+
+console.log(uniqueNumbers);
